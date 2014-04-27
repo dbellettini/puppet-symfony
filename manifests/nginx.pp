d9b540c4ad0d03cac1b24a56514783245eb644ef
Davide Bellettini <davide.bellettini@gmail.com>
2014-04-27 21:17:58 +0200

    Completing app config

--- manifests/nginx.pp
+++ manifests/nginx.pp
@@ -1,10 +1,18 @@
 define symfony::nginx (
-  $app_name = undef,
-  $app_path = undef,
-  $enable   = false,
+  $app_name    = undef,
+  $server_name = undef,
+  $web_path    = undef,
+  $enable      = true,
 ){
   file { "/etc/nginx/sites-available/$app_name": 
     ensure => present,
     content => template('symfony/nginx/site.erb')
+  }
+
+  if($enable) {
+    file { "/etc/nginx/sites-enabled/$app_name": 
+      ensure => link,
+      target => "/etc/nginx/sites-available/$app_name",
+    }
   }
 }
