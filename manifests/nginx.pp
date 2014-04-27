5d3b390c1ed0139e2c170ab7f70e279b45a9a916
Davide Bellettini <davide.bellettini@gmail.com>
2014-04-27 21:03:54 +0200

    Defining symfony::nginx class

--- manifests/nginx.pp
+++ manifests/nginx.pp
@@ -1,2 +1,10 @@
-class symfony::nginx {
+define symfony::nginx (
+  $app_name = undef,
+  $app_path = undef,
+  $enable   = false,
+){
+  file { "/etc/nginx/sites-available/$app_name": 
+    ensure => present,
+    content => template('nginx/template.erb')
+  }
 }
