d607f1c413025f39fa5ff7551a5479cda2ba991d
Davide Bellettini <davide@bellettini.me>
2015-12-28 18:31:15 +0100

    Fix manifest

--- manifests/nginx.pp
+++ manifests/nginx.pp
@@ -3,7 +3,8 @@ define symfony::nginx (
   $server_name  = '_',
   $web_root     = undef,
   $enable       = true,
-  $fastcgi_pass = 'unix:/var/run/php5-fpm.sock'
+  $fastcgi_pass = 'unix:/var/run/php5-fpm.sock',
+  $ssl_enabled  = false
 ){
   $available = "/etc/nginx/sites-available/$app_name"
   file {$available:
